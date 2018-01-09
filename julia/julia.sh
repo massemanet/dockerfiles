f89dcf01d4ad8abd56a850717028ecf4937ab417
mats cronqvist <masse@cronqvi.st>
2018-01-09 12:02:16 +0100

    get rid (mostly) of versions

--- julia/julia.sh
+++ julia/julia.sh
@@ -27,24 +27,36 @@ function xconf() {
             IP=$(ifconfig | grep 'inet '| awk '{print $2}' | grep -v 127.0.0.1)
             [ $(echo "$IP" | wc -w) == 1 ] || err "multiple IP addr: $IP"
             echo "-e DISPLAY=$IP:0 \
-                  -e XAUTHORITY=/tmp/xauth -v ~/.Xauthority:/tmp/xauth"
+                  -e XAUTHORITY=/tmp/xauth -v $HOME/.Xauthority:/tmp/xauth"
             ;;
         "Linux")
-            echo "-v /tmp/.X11-unix:/tmp/.X11-unix \
-                  -e DISPLAY=unix$DISPLAY"
+            echo "-e DISPLAY=unix$DISPLAY \
+                  -v /tmp/.X11-unix:/tmp/.X11-unix"
             ;;
     esac
+}
+
+function vsn() {
+    VSN=$(docker images | grep julia | awk '{print $2}' | sort -V | tail -1)
+    [ -z "$VSN" ] && err "no julia image, build first."
+    echo "$VSN"
 }
 
 function go() {
     INTERACTIVE="$1"
     PORT="$2"
     CMD="$3"
+    VSN=$(vsn)
     check docker
     check_X
     docker run --rm $INTERACTIVE $PORT $(xconf) -v /tmp/julia:/home/julia \
-           julia:0.6.1 \
+           julia:$VSN \
            $CMD
+}
+
+function tarball() {
+    DLPAGE=https://julialang.org/downloads
+    curl -sL $DLPAGE | grep -oE "https://[ -~]+linux-x86_64.tar.gz" | sort -u
 }
 
 CONDAPATH="/root/.julia/v0.6/Conda/deps/usr/bin"
@@ -64,10 +76,10 @@ case "$1" in
         go "-d" "-p 8888:8888" "$CONDAPATH/jupyter notebook $ARGS"
         ;;
     "build")
-        JULIA_URL=https://julialang-s3.julialang.org/bin/linux/x64/0.6/
-        JULIA_TARBALL=julia-0.6.2-linux-x86_64.tar.gz
-        BUILDARG="--build-arg=JULIA_TARBALL=JULIA_URL/$JULIA_TARBALL"
-        docker build --rm $BUILDARG -t julia:0.6.2 $(dirname $0)
+        TARBALL=$(tarball)
+        VSN=$(echo $TARBALL | grep -o "[0-9]\.[0-9]\.[0-9]")
+        BUILDARG="--build-arg JULIA_TARBALL=$TARBALL"
+        docker build --rm $BUILDARG -t julia:$VSN $(dirname $0)
         ;;
     *)
         err "unrecognized command: $1"
