092f43c8db408c9a6ea8a96fd936d452285161ce
mats cronqvist <masse@cronqvi.st>
2018-01-09 21:43:02 +0100

    removed some subshells

--- julia/julia.sh
+++ julia/julia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ue
+set -ueTE
 
 function err() {
     echo "$1"
@@ -9,14 +9,14 @@ function err() {
 
 function check() {
     echo checking "$1"
-    $(type "$1" &>/dev/null) || err "please install $1"
+    type "$1" &>/dev/ || err "please install $1"
 }
 
 function check_X() {
     case "$(uname)" in
         "Darwin")
             check Xquartz
-            $(ps -ef | grep -q "bin/Xquart[z]") || open -Fga Xquartz.app
+            ps -ef | grep -q "bin/Xquart[z]" || open -Fga Xquartz.app
             ;;
         "Linux")
             ;;
@@ -24,39 +24,36 @@ function check_X() {
 }
 
 function darwin_ip() {
-    IP=$(ifconfig | grep 'inet '| awk '{print $2}' | grep -v 127.0.0.1)
-    if [ $(echo "$IP" | wc -w) == 1 ]; then
-        echo $IP
-    else
-        err "multiple IP addr: $IP"
-    fi
+    declare -n r=$1
+    r=$(ifconfig | grep 'inet '| awk '{print $2}' | grep -v 127.0.0.1 | tail -1)
 }
 
 function xconf() {
+    declare -n r=$1
     case "$(uname)" in
         "Darwin")
-            IP=$(darwin_ip)
-            echo "-e DISPLAY=$IP:0 \
-                  -e XAUTHORITY=/tmp/xauth -v $HOME/.Xauthority:/tmp/xauth"
+            darwin_ip IP
+            r="-e DISPLAY=$IP:0 \
+               -e XAUTHORITY=/tmp/xauth -v $HOME/.Xauthority:/tmp/xauth"
             ;;
         "Linux")
-            echo "-e DISPLAY=unix$DISPLAY \
-                  -v /tmp/.X11-unix:/tmp/.X11-unix"
+            r="-e DISPLAY=unix$DISPLAY \
+               -v /tmp/.X11-unix:/tmp/.X11-unix"
             ;;
     esac
 }
 
 function vsn() {
-    VSN=$(docker images | grep julia | awk '{print $2}' | sort -V | tail -1)
-    [ -z "$VSN" ] && err "no julia image, build first."
-    echo "$VSN"
+    declare -n r=$1
+    r=$(docker images | grep julia | awk '{print $2}' | sort -V | tail -1)
+    [ -z "$r" ] && err "no julia image, build first."
 }
 
 function go() {
     FLAGS="$1"
     CMD="$2"
-    VSN=$(vsn)
-    XCONF=$(xconf)
+    vsn VSN
+    xconf XCONF
     check docker
     check_X
     docker run --rm $FLAGS $XCONF -v /tmp/julia:/home/julia \
@@ -65,8 +62,9 @@ function go() {
 }
 
 function tarball() {
+    declare -n r=$1
     DLPAGE=https://julialang.org/downloads
-    curl -sL $DLPAGE | grep -oE "https://[ -~]+linux-x86_64.tar.gz" | sort -u
+    r=$(curl -sL $DLPAGE | grep -oE "https://[^\"]+linux-x86_64.tar.gz" | sort -u)
 }
 
 CONDAPATH=/opt/conda/bin
@@ -87,7 +85,7 @@ case "$1" in
         go "-d -p 8888:8888" "$CONDAPATH/jupyter notebook $ARGS"
         ;;
     "build")
-        TARBALL=$(tarball)
+        tarball TARBALL
         VSN=$(echo $TARBALL | grep -o "[0-9]\.[0-9]\.[0-9]")
         BUILDARG="--build-arg JULIA_TARBALL=$TARBALL"
         docker build --rm $BUILDARG -t julia:$VSN $(dirname $0)
