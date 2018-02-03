276da67393c9d80548bb6cace770409114ecc177
mats cronqvist <masse@cronqvi.st>
2018-02-03 22:59:18 +0100

    some wsl(windows) support

--- helpers.sh
+++ helpers.sh
@@ -8,64 +8,76 @@ function err() {
 }
 
 function check() {
-    echo -n "checking ${1}..."
-    type "$1" &>/dev/null || err "please install $1"
+    echo -n "checking $1..."
+    type "$1" &>/dev/null || err "fail. please install $1"
     echo " ok"
 }
 
-function check_X() {
-    case "$(uname)" in
-        "Darwin")
-            check Xquartz
-            xhost +127.0.0.1
-            defaults write org.macosforge.xquartz.X11 app_to_run /usr/bin/true
-            defaults write org.macosforge.xquartz.X11 no_auth 1
-            defaults write org.macosforge.xquartz.X11 nolisten_tcp 0
-            ps -ef | grep -q "bin/Xquart[z]" || open -Fga Xquartz.app
-            ;;
-        "Linux")
-            ;;
-    esac
-}
-
 function xconf() {
-    local r
+    local r=""
     case "$(uname)" in
         "Darwin")
-            r="-e DISPLAY=docker.for.mac.host.internal:0 \
-               -v /tmp/.X11-unix:/tmp/.X11-unix"
+            if type Xquartz &> /dev/null; then
+                xhost +127.0.0.1
+                defaults write org.macosforge.xquartz.X11 app_to_run /usr/bin/true
+                defaults write org.macosforge.xquartz.X11 no_auth 1
+                defaults write org.macosforge.xquartz.X11 nolisten_tcp 0
+                ps -ef | grep -q "bin/Xquart[z]" || open -Fga Xquartz.app
+                r="-e DISPLAY=docker.for.mac.host.internal:0 \
+                   -v /tmp/.X11-unix:/tmp/.X11-unix"
+            else
+                echo "You don't have Xquartz. Disabling X"
+            fi
             ;;
         "Linux")
-            r="-e DISPLAY=unix$DISPLAY \
-               -v /tmp/.X11-unix:/tmp/.X11-unix"
+            if [ -n "$DISPLAY" ]; then
+                r="-e DISPLAY=unix$DISPLAY \
+                   -v /tmp/.X11-unix:/tmp/.X11-unix"
+            else
+                echo "You don't have X. SAD!"
+            fi
             ;;
     esac
     eval $1="'$r'";
 }
 
-function find_tag() {
+function find_image() {
     local r
     local TARGET="$2"
-    r=$(docker images | grep $TARGET | awk '{print $2}' | sort -V | tail -1)
+    r=$(docker images | grep "$TARGET" |\
+        awk '{print $2,$3}' | sort -V | tail -1 | cut -f2 -d" ")
     [ -z "$r" ] && err "no $TARGET image, build first."
     eval $1="'$r'";
 }
 
+function flags() {
+    local r
+    local TARGET="$2"
+    local VOL="$3"
+    local DETACH="--detach-keys ctrl-q,ctrl-q"
+    local WRKDIR="/opt/$TARGET/tmp"
+    if uname -a | grep -q 'Microsoft'; then
+        VOL="$(sed 's|/mnt/\([a-z]\)|\1:|' <<< $VOL)"
+        r="$DETACH -v \"$VOL\":$WRKDIR"
+    else
+        r="--rm $DETACH -v \"$VOL\":$WRKDIR"
+    fi
+    eval $1="'$r'";
+}
+
 function go() {
-    local TAG XCONF
     local TARGET="$1"
     local USERFLAGS="$2"
     local CMD="$3"
     local VOL="$4"
-    local FLAGS="--detach-keys ctrl-q,ctrl-q --rm -v ${VOL}:/opt/$TARGET/tmp"
-    check docker
-    check_X
-    find_tag TAG $TARGET
-    xconf XCONF
     mkdir -p "$VOL"
-    echo "found $TARGET:$TAG"
+    check docker
+    find_image IMAGE "$TARGET"
+    xconf XFLAGS
+    flags FLAGS "$TARGET" "$VOL"
+    echo "found image $IMAGE"
     echo "mounting $VOL"
-    docker run $FLAGS $USERFLAGS $XCONF $TARGET:$TAG $CMD
+    eval docker run "$FLAGS $USERFLAGS $XFLAGS $IMAGE $CMD"
 }
 
 function image() {
