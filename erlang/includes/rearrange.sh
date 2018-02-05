72b3fc6366483c351a685be1df5c4f724fd60430
mats cronqvist <masse@cronqvi.st>
2018-02-05 23:44:07 +0100

    fix typo

--- erlang/includes/rearrange.sh
+++ erlang/includes/rearrange.sh
@@ -2,8 +2,8 @@
 
 echo "$(id) : $HOME"
 cd /opt/includes || exit 1
-mkdir "$HOME/.emacs.d/fdlcap"
 cp -r .emacs.d/ "$HOME"
+mkdir "$HOME/.emacs.d/fdlcap"
 cp bashrc "$HOME/.bashrc"
 cp erlang "$HOME/.erlang"
 cp fdlcap.el "$HOME/.emacs.d/fdlcap"
