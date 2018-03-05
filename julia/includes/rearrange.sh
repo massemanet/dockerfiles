fdf8852cbdcc716d23dcce2316d88198f2f3b010
mats cronqvist <masse@cronqvi.st>
2018-03-05 09:41:23 +0100

    safelinking

--- julia/includes/rearrange.sh
+++ julia/includes/rearrange.sh
@@ -2,7 +2,11 @@
 
 echo "$(id) : $HOME"
 
-ln -s /root/.[c-k]* ~
+function safelink() { [ -e "$1" ] || ln -s "$1" "$2"; }
+
+for d in /root/.[c-k]* ; do
+    safelink "$d" ~
+done
 
 cd /opt/includes || exit 1
 sudo cp output.jl /opt/julia_local/Plots/src
