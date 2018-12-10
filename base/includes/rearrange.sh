d47c2f0172d5a9780acc2c5dd0244dc023cadaf5
mats cronqvist <masse@cronqvi.st>
2018-12-10 13:56:49 +0100

    intellij configs

--- base/includes/rearrange.sh
+++ base/includes/rearrange.sh
@@ -12,15 +12,18 @@ copy() {
     fi
 }
 
-# if $1 exists, and dirname($2) exists or can be created,
-# and $2 does not exist, link $2 -> $1
+# if $1 exists, and dirname($2) exists or can be created, then
+#   if $1 is a regular file, and $2 does not exist, link $2 -> $1
+#   if $1 is a dir, and $2 is a dir or does not exist, link $1/* -> $2
 # $2 will not be overwritten
 link() {
     [ ! -e "$1" ] && return 1
     [ ! -e "$(dirname "$2")" ] && ! mkdir -p "$(dirname "$2")" && return 2
-    [ -e "$2" ] && return 3
-    ln -s "$1" "$2"
-
+    [ -d "$1" ] && ! mkdir -p "$2" && return 3
+    if [ -d "$1" ] && [ -d "$2" ]
+    then find "$1" -maxdepth 1 -mindepth 1 -exec ln -s {} "$2" \;
+    else ln -s "$1" "$2"
+    fi
 }
 
 cd "$(dirname "$0")" || exit 1
